63c740f8c7b3c316f167c5196be412bb0c82b78e
mubashirali126 <alimubashir205@yahoo.com>
2023-07-18 05:14:40 -0700

    create a gist

--- src/services/gistService.js
+++ src/services/gistService.js
@@ -3,6 +3,10 @@ import { Octokit } from '@octokit/rest';
 
 const octokit = new Octokit();
 
+// const octokit = new Octokit ({	
+//   auth: "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"	
+// });
+
 const GistService = ({ username }) => {
   const [gists, setGists] = useState([]);
 
@@ -22,5 +26,15 @@ const GistService = ({ username }) => {
 
   return { gists };
 };
+
+// Create a gist
+// octokit.rest.gists.create({	
+//   files: {	
+//     'README.md': {	
+//       content: 'Hello World'	
+//     }	
+//   }	
+//   })	
+//   .then((response) => console.log("Gist is created"));
 
 export default GistService;
